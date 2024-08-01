f3901af5327d5ee74aa383d23f22814dc3b3c331
Tharun013 <155997984+Mastertharun@users.noreply.github.com>
2024-08-01 05:44:31 +0000

    react props--contacts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-import App from "./App";
-
-ReactDom.render(<h1>hello</h1>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
