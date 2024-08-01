ef0fcfe9c0c47806ab60ca61ebf340192e244850
Tharun013 <155997984+Mastertharun@users.noreply.github.com>
2024-08-01 06:21:11 +0000

    emojipedia

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
-
+import App from "./components/App1";
+import "./styles.css";
 ReactDOM.render(<App />, document.getElementById("root"));
